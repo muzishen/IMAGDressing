656a793a3572935ebb11a16589ca53d1e5041345
FAREAST\chaoqunzhao <chaoqunzhao@microsoft.com>
2024-07-11 15:08:46 +0800

    Fix some errors

--- inference_IMAGdressing_controlnetinpainting.py
+++ inference_IMAGdressing_controlnetinpainting.py
@@ -65,7 +65,7 @@ def prepare(args):
     tokenizer = CLIPTokenizer.from_pretrained("SG161222/Realistic_Vision_V4.0_noVAE", subfolder="tokenizer")
     text_encoder = CLIPTextModel.from_pretrained("SG161222/Realistic_Vision_V4.0_noVAE", subfolder="text_encoder").to(
         dtype=torch.float16, device=args.device)
-    image_encoder = CLIPVisionModelWithProjection.from_pretrained("h94/IP-Adapter", subfolder="image_encoder").to(
+    image_encoder = CLIPVisionModelWithProjection.from_pretrained("h94/IP-Adapter", subfolder="models/image_encoder").to(
         dtype=torch.float16, device=args.device)
     unet = UNet2DConditionModel.from_pretrained("SG161222/Realistic_Vision_V4.0_noVAE", subfolder="unet").to(
         dtype=torch.float16,
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     parser.add_argument('--cloth_path', type=str, required=True)
     parser.add_argument('--model_path', type=str, required=True)
     parser.add_argument('--output_path', type=str, default="./output_sd_inpaint")
-    parser.add_argument('--device', type=str, default="cuda:0")
+    parser.add_argument('--device', type=str, default="cuda:1")
     args = parser.parse_args()
 
     # svae path
@@ -178,8 +178,8 @@ if __name__ == "__main__":
     # prepare pipline
     pipe, generator = prepare(args)
     # prepare mask model
-    parsing_model = Parsing(0)
-    openpose_model = OpenPose(0)
+    parsing_model = Parsing(1)
+    openpose_model = OpenPose(1)
     print('====================== pipe load finish ===================')
 
     num_samples = 1
